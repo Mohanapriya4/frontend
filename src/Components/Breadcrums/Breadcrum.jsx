a11eb4b6d3bcc9b2036b57a881dc52bc157bc399
Mohanapriya4 <73380833+Mohanapriya4@users.noreply.github.com>
2024-12-05 18:19:17 +0530

    Cart

--- src/Components/Breadcrums/Breadcrum.jsx
+++ src/Components/Breadcrums/Breadcrum.jsx
@@ -8,7 +8,6 @@ const Breadcrum = (props) => {
   return (
     <div className='breadcrum'>
         HOME <img src={arrow_icon} alt=''/> SHOP <img src={arrow_icon} alt=''/> {product.category} <img src={arrow_icon} alt=''/> {product.name} <img src={arrow_icon} alt=''/>
-
     </div>
   )
 }
